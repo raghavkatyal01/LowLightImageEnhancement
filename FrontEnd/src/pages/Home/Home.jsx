8efc94d0b1bb4ed072fde9bca2363c4d82ab9b56
Manasvi Gaur <manasvigaur3@gmail.com>
2024-08-31 20:12:22 +0530

    moon2

--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -1,17 +1,22 @@
 // Home.js
-import React from 'react';
-import './Home.css';
-import Globe from 'react-globe.gl';
-import moonTexture from '../../Image/moon.jpeg'; // Use your downloaded image
+import React from "react";
+import "./Home.css";
+import Globe from "react-globe.gl";
+import moonTexture from "../../Image/moon.jpeg"; // Use your downloaded image
 
 function Home() {
   return (
     <>
-
       <div className="mainpage">
         <div className="page1">
-          <span>Welcome to GeekPie</span>
+          <div style={{ width: "100%", height: "100%" }}>
+            <Globe
+              globeImageUrl={moonTexture}
+              backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+            />
+          </div>
         </div>
+
         <div className="page2">
           <span>Page 2</span>
         </div>
@@ -20,13 +25,6 @@ function Home() {
         </div>
         <div className="page4">
           <span>Page 4</span>
-        </div>
-        <div className="page5">
-          <span>Page 5</span>
-          <div style={{ width: '100%', height: '100%' }}>
-            <Globe globeImageUrl={moonTexture}
-        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"/>
-          </div>
         </div>
       </div>
     </>
