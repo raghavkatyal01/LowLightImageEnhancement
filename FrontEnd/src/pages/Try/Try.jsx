476ab227d5e49b5545df5881b7501617db74f44b
Manasvi Gaur <manasvigaur3@gmail.com>
2024-09-01 04:45:55 +0530

    ff

--- FrontEnd/src/pages/Try/Try.jsx
+++ FrontEnd/src/pages/Try/Try.jsx
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from "react";
 import "./Try.css";
 import stars from "../../assets/stars.jpg";
 import stars1 from "../../assets/stars.png";
+import scroll from "../../Image/scroll.jpg"; 
 import clouds from "../../assets/clouds.png";
 import moon from "../../assets/moon.png";
+import ModelViewer from "../../components/crat3d/ModelViewer";
 
 function Try() {
   const [isButton, setIsButton] = useState(false);
+  const scrollToSection = () => {
+    const section = document.getElementById('page_2');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -65,19 +73,44 @@ function Try() {
             <img src={moon} alt="moon" />
           </button>
           <div className="text-container">
-            <p>
-                Explore the Mysteries of the Night Sky
-              Dive deep into the cosmos and discover the secrets of the universe. 
-              Whether you are a beginner or an expert, there is always something new to learn.
+            <p className="heading">
+            The Moon is magic for the soul and light for the senses..
             </p>
+            
           </div>
+          <button
+            onClick={scrollToSection}
+            style={{
+              position: "fixed",
+              bottom: "20px",
+              right: "20px",
+              padding: "10px 10px",
+              backgroundColor: "Transparent",
+              color: "white",
+              border: "none",
+              borderRadius: "545px",
+              cursor: "pointer",
+              zIndex: "5"
+            }}
+          >
+            <img
+              src={scroll}
+              alt="Scroll to Section"
+              style={{ width: "50px", height: "50px" }}
+            />
+          </button> 
         </div>
-        <div className="page_2">
+
+        <div id="page_2" className="page_2">
           {/* Content for page 2 */}
+          <div style={{ width: '40vw', height: '30vh', zIndex:'10'}}>
+          </div>
           <div className="stars1">
             <img src={stars1} alt="stars" />
+            
           </div>
         </div>
+        
 
       </div>
     </>
