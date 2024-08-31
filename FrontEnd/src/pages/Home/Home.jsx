e5c7f83c875565fe47de120490d7c0f4bc533baa
Shivam Kumar Pathak <114474974+Shivampathak-1@users.noreply.github.com>
2024-08-31 18:23:52 +0530

    #1

--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-      Hello this is Home.
-    </div>
-  )
+    <>
+      <div className="mainpage">
+        <div className="page1">
+
+        </div>
+        <div className="page2">Page 2</div>
+        <div className="page3">Page 3</div>
+        <div className="page4">Page 4</div>
+        <div className="page5">Page 5</div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
