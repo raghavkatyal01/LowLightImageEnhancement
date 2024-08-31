1c440313c8c4d3dc073c24fde8872f07711e55f3
Manasvi Gaur <manasvigaur3@gmail.com>
2024-08-31 20:11:18 +0530

    moon

--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 // Home.js
 import React from 'react';
 import './Home.css';
+import Globe from 'react-globe.gl';
+import moonTexture from '../../Image/moon.jpeg'; // Use your downloaded image
 
 function Home() {
   return (
@@ -21,6 +23,10 @@ function Home() {
         </div>
         <div className="page5">
           <span>Page 5</span>
+          <div style={{ width: '100%', height: '100%' }}>
+            <Globe globeImageUrl={moonTexture}
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"/>
+          </div>
         </div>
       </div>
     </>
